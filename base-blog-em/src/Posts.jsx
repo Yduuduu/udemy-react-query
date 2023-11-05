13d12ba574440bb0a4c8c283f9b663727ea4fe01
Yduuduu <hou_o@naver.com>
2023-11-05 12:12:36 +0900

    🎉 강의 코드 및 base-blog-em 실습

---
+++ base-blog-em/src/Posts.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { PostDetail } from "./PostDetail";
+import { useQuery, useQueryClient } from "react-query";
+const maxPostPage = 10;
+
+async function fetchPosts(pageNum) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts"], () => fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClient]);
+
+  // replace with useQuery
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true,
+    }
+  );
+  if (isLoading) return <div>Loading...</div>;
+  if (isError)
+    return (
+      <>
+        <div>Oops, something went wrong</div>
+        <p>{error.toString()}</p>
+      </>
+    );
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
+          Previous page
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
